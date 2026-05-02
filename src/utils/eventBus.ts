a066fb23a36a26ffe891557160074f4b977a9e3f
Mohammad.Hassan.Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2026-05-02 16:48:45 +0330

    chore(eventBus): add reference comments for available events

--- src/utils/eventBus.ts
+++ src/utils/eventBus.ts
@@ -3,3 +3,13 @@ import { EventEmitter } from "events";
 class EventBus extends EventEmitter {}
 
 export const eventBus = new EventBus();
+
+// List of available events for reference:
+// - order:created
+// - order:filled
+// - order:rejected
+// - order:cancelled
+// - order:updated
+// - position:opened
+// - position:closed
+// - trade:executed
